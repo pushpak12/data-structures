2c5c1e3e27ada32bedcf81b564ba0924fb5b1dce
Pushpak Katkar <pushpakkatkar@gmail.com>
2024-02-22 17:49:17 +0530

    First commit

---
+++ dsa-learnings/src/dsa/learning/basics/HelloWorld.java
@@ -0,0 +1,10 @@
+package dsa.learning.basics;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
